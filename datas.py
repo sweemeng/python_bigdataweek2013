3739acf08b5318ae4cfe8e5e84717f349bd89cde
sweemeng <sweester@gmail.com>
2013-07-22 20:58:10 +0800

    reading data in new format

--- datas.py
+++ datas.py
@@ -1,23 +1,23 @@
 import pandas
 
 
-training = pandas.read_csv("data/train.csv")
-testing  = pandas.read_csv("data/test.csv")
+training = pandas.read_csv("data/train.csv", index_col="PassengerId")
+testing  = pandas.read_csv("data/test.csv", index_col="PassengerId")
 
 def clean_data(data_frame):
     data_frame = data_frame.drop(
-        ['name','cabin','ticket','parch'],axis=1)
-    data_frame['sex'] = data_frame['sex'].replace(
+        ['Name','Cabin','Ticket','Parch'],axis=1)
+    data_frame['Sex'] = data_frame['Sex'].replace(
         ['male','female'], [1,0])
-    data_frame['embarked'] = data_frame['embarked'].replace(
+    data_frame['Embarked'] = data_frame['Embarked'].replace(
        ['C','S','Q'], [0,1,2])
     
-    data_frame['age'] = data_frame['age'].fillna(data_frame['age'].mean())
-    data_frame['embarked'] = data_frame['embarked'].fillna(
-        data_frame['embarked'].mean())
+    data_frame['Age'] = data_frame['Age'].fillna(data_frame['Age'].mean())
+    data_frame['Embarked'] = data_frame['Embarked'].fillna(
+        data_frame['Embarked'].mean())
 
-    data_frame['fare'] = data_frame['fare'].fillna(
-        data_frame['fare'].mean())
+    data_frame['Fare'] = data_frame['Fare'].fillna(
+        data_frame['Fare'].mean())
 
     return data_frame
 
